8898ae39f5f053086668684e33045721e80c7b03
Kushal Jain <jkushal069@gmail.com>
2023-06-29 15:07:55 +0530

    fixing production issue

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -76,7 +76,7 @@ const Modal = ({
       <div class="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto">
         <div class="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]">
           <div class="flex justify-between items-center py-3 px-4 border-b dark:border-gray-700">
-            <h3 class="font-bold text-gray-800 dark:text-white">Modal title</h3>
+            <h3 class="font-bold text-gray-800 dark:text-white">Add contact</h3>
             <button
               type="button"
               onClick={() => {
