36d5745843782e470b0ee5e9fc9ff0ff7890d1f8
Kushal Jain <jkushal069@gmail.com>
2023-06-29 16:25:27 +0530

    gh-pages deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Contact-app2-coding-ninjas",
+  base: "/Contact-app2-coding-ninjas/",
   plugins: [react()],
 });
